120dcefefc65caa4771cafaa121d2357d9af3187
cf16 <cf16rocket@gmail.com>
2014-04-27 13:00:21 +0200

    three way handshake

--- ibclient_2_connect/main.cpp
+++ ibclient_2_connect/main.cpp
@@ -15,54 +15,65 @@ int
 main( int argc, char **argv) {
     int sockfd, n;
     int rc = 0;
+    char buff[ MAXLINE + 1];
     char recvline[ MAXLINE + 1];
     struct sockaddr_in servaddr;
+    int port = 7496;
 
-    if (argc != 2)
+    if ( argc != 2)
         err_quit("usage: a.out <IPaddress>");
 
     sockfd = Socket(AF_INET, SOCK_STREAM, 0);
 
     bzero( &servaddr, sizeof (servaddr));
     servaddr.sin_family = AF_INET;
-    servaddr.sin_port = htons( 9997); /* test */
+    servaddr.sin_port = htons( port); /* test */
     
     if ( inet_pton( AF_INET, argv[1], &servaddr.sin_addr) <= 0)
         err_quit("inet_pton error for %s", argv[1]);
 
     /* starting three way handshake*/
-    fprintf( stderr, "starting three way handshake");
+    fprintf( stderr, "starting three way handshake\n");
     if( connect( sockfd, (SA *) & servaddr, sizeof (servaddr)) < 0) {
         if( errno == ETIMEDOUT) {
-            fprintf( stderr, "no response to SYN segment");
+            fprintf( stderr, "connection timed out, no response to SYN segment\n");
         }
+        if( errno == ECONNREFUSED) {
+            fprintf( stderr, "no process listening on port %d\n", port);
+        }
+        if( errno == EHOSTUNREACH || errno == ENETUNREACH) {
+            fprintf( stderr, "can't reach the server\n", port);
+        }
+        err_sys( "connect error");
     }
-
-    int counter = 0;
-    while ( ( n = read(sockfd, recvline, MAXLINE)) > 0) {
-        counter = counter + 1;
+    fprintf( stderr, "connected\n");
+    
+    int myInt = 60;    
+    int tmp = htonl( (uint32_t)myInt);
+    Write( sockfd, &tmp, sizeof(tmp));
+    
+    
+        if ( (n = read(sockfd, recvline, MAXLINE)) > 0) {
         recvline[n] = 0; /* null terminate */
         //printf( "%s", recvline);
-        if (fputs(recvline, stdout) == EOF)
-            err_sys("fputs error");
+        if ( fputs(recvline, stdout) == EOF)
+            err_sys( "fputs error");
         /*
         Since the stream is fully-buffered by default, not line-buffered,
         it needs to be flushed periodically.  We'll flush it here for
         demonstration purposes, even though we're about to close it.
          */
-        int flush_status = fflush(stdout);
+        int flush_status = fflush( stdout);
         if ( flush_status != 0) {
-            puts("\nError flushing stream!");
+            puts( "\nError flushing stream!");
         } else {
-            puts("\nStream flushed.");
+            puts( "\nStream flushed.");
         }
-        //printf( "counter:%d", counter);
     }
-    
-    printf( "counter:%d\n", counter);
     if (n < 0)
-        err_sys("read error");
-
+        err_sys( "read error");
+    
+    fprintf( stderr, "exiting\n");
     exit(0);
 }
 
