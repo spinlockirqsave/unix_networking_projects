0e4e1a38559b371d8e660a0a561f3ebb6ca0384e
cf16 <cf16rocket@gmail.com>
2014-04-27 18:39:40 +0200

    connection establishment, blocking socket

--- ibclient_2_connect/main.cpp
+++ ibclient_2_connect/main.cpp
@@ -9,8 +9,12 @@ extern "C" {
     #include "../../../include/unix_networking/unpv13e/lib/unp.h"
 }
 
+#include "twsapi_defs.h" 
+
 #include <time.h>
 #include <assert.h>
+#include <sstream>
+#include <string>
 
 /**
  * Resolve host names.
@@ -40,6 +44,15 @@ static int resolveHost( const char *host, unsigned int port, int family,
 	int s = getaddrinfo(host, strport, &hints, res);
 	return s;
 }
+
+const char* findFieldEnd( const char* ptr, const char* endPtr) {
+	return (const char*) memchr( ptr, 0, endPtr - ptr);
+}
+
+void decodeField( int& intValue, const char* ptr) {
+    intValue = atoi( ptr);
+}
+
 
 int
 main( int argc, char **argv) {
@@ -96,54 +109,55 @@ main( int argc, char **argv) {
         /* successfully  created */
         break;
     }
-        
 
-//    sockfd = Socket(AF_INET, SOCK_STREAM, 0);
-//
-//    bzero( &servaddr, sizeof (servaddr));
-//    servaddr.sin_family = AF_INET;
-//    servaddr.sin_port = htons( port); /* test */
-//    
-//    if ( inet_pton( AF_INET, argv[1], &servaddr.sin_addr) <= 0)
-//        err_quit("inet_pton error for %s", argv[1]);
-//
-//    /* starting three way handshake*/
-//    fprintf( stderr, "starting three way handshake\n");
-//    if( connect( sockfd, (SA *) & servaddr, sizeof (servaddr)) < 0) {
-//        if( errno == ETIMEDOUT) {
-//            fprintf( stderr, "connection timed out, no response to SYN segment\n");
-//        }
-//        if( errno == ECONNREFUSED) {
-//            fprintf( stderr, "no process listening on port %d\n", port);
-//        }
-//        if( errno == EHOSTUNREACH || errno == ENETUNREACH) {
-//            fprintf( stderr, "can't reach the server\n", port);
-//        }
-//        err_sys( "connect error");
-//    }
     fprintf( stderr, "connected\n");
     
     const int CLIENT_VERSION = 60;
-    int tmp = htonl( (uint32_t)CLIENT_VERSION);
-    Write( sockfd, &tmp, sizeof(tmp));
+    //int tmp = htonl( (uint32_t)CLIENT_VERSION);
+    std::ostringstream oss;
+    oss << CLIENT_VERSION << '\0';
+    std::string str = oss.str();
+    Write( sockfd, (void*)str.data(), sizeof( str.data()));
     
     
         while ( (n = read(sockfd, recvline, MAXLINE)) > 0) {
         recvline[n] = 0; /* null terminate */
-        //printf( "%s", recvline);
-        if ( fputs(recvline, stdout) == EOF)
-            err_sys( "fputs error");
-        /*
-        Since the stream is fully-buffered by default, not line-buffered,
-        it needs to be flushed periodically.  We'll flush it here for
-        demonstration purposes, even though we're about to close it.
-         */
-        int flush_status = fflush( stdout);
-        if ( flush_status != 0) {
-            puts( "\nError flushing stream!");
-        } else {
-            puts( "\nStream flushed.");
+        printf( "n=%d,", n);
+        int decoded;
+        if( findFieldEnd( &recvline[0], &recvline[n]) != 0) {
+            
+            decodeField( decoded, &recvline[0]);
+            printf( "decoded=%d,", decoded);
+            
+            if( decoded == MANAGED_ACCTS) {
+                 printf( "MANAGED_ACCTS,");
+            }
+            if( decoded == NEXT_VALID_ID) {
+                 printf( "NEXT_VALID_ID,");
+            }
+            if( decoded == ERR_MSG) {
+                 printf( "ERR_MSG,");
+            }
         }
+
+        for( int i = 0; i < n + 1; ++i) {
+            printf( "%c", recvline[ i]);
+        }
+        printf( "\n");
+        if( decoded == ERR_MSG) {
+             break;
+        }
+//        /*
+//        Since the stream is fully-buffered by default, not line-buffered,
+//        it needs to be flushed periodically.  We'll flush it here for
+//        demonstration purposes, even though we're about to close it.
+//         */
+         int flush_status = fflush( stdout);
+//        if ( flush_status != 0) {
+//            puts( "\nError flushing stream!");
+//        } else {
+//            puts( "\nStream flushed.");
+//        }
     }
     if (n < 0)
         err_sys( "read error");
