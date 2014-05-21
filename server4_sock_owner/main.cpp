d621823a041949159b4338950c476f434885ffb5
cf16 <cf16rocket@gmail.com>
2014-05-21 21:05:10 +0200

    SIGIO

---
+++ server4_sock_owner/main.cpp
@@ -0,0 +1,615 @@
+/* 
+ * File:   main.cpp
+ * Author: piter cf16 eu
+ *
+ * Created on May 21, 2014, 8:00 PM
+ */
+
+#include <stdio.h>
+#include <errno.h>
+#include	<stdarg.h>		/* ANSI C header file */
+#include	<syslog.h>		/* for syslog() */
+#include <string.h>
+#include <stdlib.h>
+
+#include <unistd.h>
+#include	<sys/types.h>
+#include        <sys/wait.h>            /* for waitpid */
+#include	<sys/socket.h>
+#include	<netinet/in.h>
+#include	<arpa/inet.h>
+
+#include <math.h>
+
+#include <fcntl.h>
+
+#define MAXLINE 4096
+#define SERV_PORT 9877
+#define SA struct sockaddr
+int		daemon_proc;		/* set nonzero by daemon_init() */
+
+#define	min(a,b)	((a) < (b) ? (a) : (b))
+#define	max(a,b)	((a) > (b) ? (a) : (b))
+
+#define	LISTENQ		1024	/* 2nd argument to listen() */
+
+typedef void (Sigfunc)(int);    /* signal handler */
+
+/* Print message and return to caller
+ * Caller specifies "errnoflag" and "level" */
+static void
+err_doit(int errnoflag, int level, const char *fmt, va_list ap)
+{
+	int		errno_save, n;
+	char	buf[MAXLINE + 1];
+
+	errno_save = errno;		/* value caller might want printed */
+#ifdef	HAVE_VSNPRINTF
+	vsnprintf(buf, MAXLINE, fmt, ap);	/* safe */
+#else
+	vsprintf(buf, fmt, ap);					/* not safe */
+#endif
+	n = strlen(buf);
+	if (errnoflag)
+		snprintf(buf + n, MAXLINE - n, ": %s", strerror(errno_save));
+	strcat(buf, "\n");
+
+	if (daemon_proc) {
+		syslog(level, buf);
+	} else {
+		fflush(stdout);		/* in case stdout and stderr are the same */
+		fputs(buf, stderr);
+		fflush(stderr);
+	}
+	return;
+}
+
+/* Fatal error unrelated to system call
+ * Print message and terminate */
+
+void
+err_quit(const char *fmt, ...)
+{
+	va_list		ap;
+
+	va_start(ap, fmt);
+	err_doit(0, LOG_ERR, fmt, ap);
+	va_end(ap);
+	exit(1);
+}
+
+/* Fatal error related to system call
+ * Print message and terminate */
+
+void
+err_sys(const char *fmt, ...)
+{
+	va_list		ap;
+
+	va_start(ap, fmt);
+	err_doit(1, LOG_ERR, fmt, ap);
+	va_end(ap);
+	exit(1);
+}
+
+void
+Inet_pton(int family, const char *strptr, void *addrptr)
+{
+	int		n;
+
+	if ( (n = inet_pton(family, strptr, addrptr)) < 0)
+		err_sys("inet_pton error for %s", strptr);	/* errno set */
+	else if (n == 0)
+		err_quit("inet_pton error for %s", strptr);	/* errno not set */
+
+	/* nothing to return */
+}
+
+Sigfunc *
+signal(int signo, Sigfunc *func)
+{
+	struct sigaction	act, oact;
+
+	act.sa_handler = func;
+	sigemptyset(&act.sa_mask);
+	act.sa_flags = 0;
+	if (signo == SIGALRM) {
+#ifdef	SA_INTERRUPT
+		act.sa_flags |= SA_INTERRUPT;	/* SunOS 4.x */
+#endif
+	} else {
+#ifdef	SA_RESTART
+		act.sa_flags |= SA_RESTART;		/* SVR4, 44BSD */
+#endif
+	}
+	if (sigaction(signo, &act, &oact) < 0)
+		return(SIG_ERR);
+	return(oact.sa_handler);
+}
+/* end signal */
+
+Sigfunc *
+Signal(int signo, Sigfunc *func)	/* for our signal() function */
+{
+	Sigfunc	*sigfunc;
+
+	if ( (sigfunc = signal(signo, func)) == SIG_ERR)
+		err_sys("signal error");
+	return(sigfunc);
+}
+
+int
+Socket(int family, int type, int protocol)
+{
+	int		n;
+
+	if ( (n = socket(family, type, protocol)) < 0)
+		err_sys("socket error");
+	return(n);
+}
+
+void
+Connect(int fd, const struct sockaddr *sa, socklen_t salen)
+{
+	if (connect(fd, sa, salen) < 0)
+		err_sys("connect error");
+}
+
+ssize_t
+Read(int fd, void *ptr, size_t nbytes)
+{
+	ssize_t		n;
+
+	if ( (n = read(fd, ptr, nbytes)) == -1)
+		err_sys("read error");
+	return(n);
+}
+
+void
+Write(int fd, void *ptr, size_t nbytes)
+{
+	if (write(fd, ptr, nbytes) != nbytes)
+		err_sys("write error");
+}
+
+ssize_t						/* Write "n" bytes to a descriptor. */
+writen(int fd, const void *vptr, size_t n)
+{
+	size_t		nleft;
+	ssize_t		nwritten;
+	const char	*ptr;
+
+	ptr = ( const char*) vptr;
+	nleft = n;
+	while (nleft > 0) {
+		if ( (nwritten = write(fd, ptr, nleft)) <= 0) {
+			if (nwritten < 0 && errno == EINTR)
+				nwritten = 0;		/* and call write() again */
+			else
+				return(-1);			/* error */
+		}
+
+		nleft -= nwritten;
+		ptr   += nwritten;
+	}
+	return(n);
+}
+/* end writen */
+
+void
+Writen(int fd, void *ptr, size_t nbytes)
+{
+	if (writen(fd, ptr, nbytes) != nbytes)
+		err_sys("writen error");
+}
+
+int
+Select(int nfds, fd_set *readfds, fd_set *writefds, fd_set *exceptfds,
+       struct timeval *timeout)
+{
+	int		n;
+
+	if ( (n = select(nfds, readfds, writefds, exceptfds, timeout)) < 0)
+		err_sys("select error");
+	return(n);		/* can return 0 on timeout */
+}
+
+void
+Send(int fd, const void *ptr, size_t nbytes, int flags)
+{
+	if (send(fd, ptr, nbytes, flags) != (ssize_t)nbytes)
+		err_sys("send error");
+}
+
+void
+Sendto(int fd, const void *ptr, size_t nbytes, int flags,
+	   const struct sockaddr *sa, socklen_t salen)
+{
+	if (sendto(fd, ptr, nbytes, flags, sa, salen) != (ssize_t)nbytes)
+		err_sys("sendto error");
+}
+
+void
+Sendmsg(int fd, const struct msghdr *msg, int flags)
+{
+	unsigned int	i;
+	ssize_t			nbytes;
+
+	nbytes = 0;	/* must first figure out what return value should be */
+	for (i = 0; i < msg->msg_iovlen; i++)
+		nbytes += msg->msg_iov[i].iov_len;
+
+	if (sendmsg(fd, msg, flags) != nbytes)
+		err_sys("sendmsg error");
+}
+
+void
+Setsockopt(int fd, int level, int optname, const void *optval, socklen_t optlen)
+{
+	if (setsockopt(fd, level, optname, optval, optlen) < 0)
+		err_sys("setsockopt error");
+}
+
+void
+Shutdown(int fd, int how)
+{
+	if (shutdown(fd, how) < 0)
+		err_sys("shutdown error");
+}
+
+char *
+Fgets(char *ptr, int n, FILE *stream)
+{
+	char	*rptr;
+
+	if ( (rptr = fgets(ptr, n, stream)) == NULL && ferror(stream))
+		err_sys("fgets error");
+
+	return (rptr);
+}
+
+FILE *
+Fopen(const char *filename, const char *mode)
+{
+	FILE	*fp;
+
+	if ( (fp = fopen(filename, mode)) == NULL)
+		err_sys("fopen error");
+
+	return(fp);
+}
+
+void
+Fputs(const char *ptr, FILE *stream)
+{
+	if (fputs(ptr, stream) == EOF)
+		err_sys("fputs error");
+}
+
+int
+Accept(int fd, struct sockaddr *sa, socklen_t *salenptr)
+{
+	int		n;
+
+again:
+	if ( (n = accept(fd, sa, salenptr)) < 0) {
+#ifdef	EPROTO
+		if (errno == EPROTO || errno == ECONNABORTED)
+#else
+		if (errno == ECONNABORTED)
+#endif
+			goto again;
+		else
+			err_sys("accept error");
+	}
+	return(n);
+}
+
+void
+Bind(int fd, const struct sockaddr *sa, socklen_t salen)
+{
+	if (bind(fd, sa, salen) < 0)
+		err_sys("bind error");
+}
+
+void
+Close(int fd)
+{
+	if (close(fd) == -1)
+		err_sys("close error");
+}
+
+int
+Fcntl(int fd, int cmd, int arg)
+{
+	int	n;
+
+	if ( (n = fcntl(fd, cmd, arg)) == -1)
+		err_sys("fcntl error");
+	return(n);
+}
+
+pid_t
+Fork(void)
+{
+	pid_t	pid;
+
+	if ( (pid = fork()) == -1)
+		err_sys("fork error");
+	return(pid);
+}
+
+void
+Getpeername(int fd, struct sockaddr *sa, socklen_t *salenptr)
+{
+	if (getpeername(fd, sa, salenptr) < 0)
+		err_sys("getpeername error");
+}
+
+void
+Getsockname(int fd, struct sockaddr *sa, socklen_t *salenptr)
+{
+	if (getsockname(fd, sa, salenptr) < 0)
+		err_sys("getsockname error");
+}
+
+void
+Getsockopt(int fd, int level, int optname, void *optval, socklen_t *optlenptr)
+{
+	if (getsockopt(fd, level, optname, optval, optlenptr) < 0)
+		err_sys("getsockopt error");
+}
+
+/* include Listen */
+void
+Listen(int fd, int backlog)
+{
+	char	*ptr;
+
+		/*4can override 2nd argument with environment variable */
+	if ( (ptr = getenv("LISTENQ")) != NULL)
+		backlog = atoi(ptr);
+
+	if (    listen(fd, backlog) < 0)
+		err_sys("listen error");
+}
+/* end Listen */
+
+#ifdef	HAVE_POLL
+int
+Poll(struct pollfd *fdarray, unsigned long nfds, int timeout)
+{
+	int		n;
+
+	if ( (n = poll(fdarray, nfds, timeout)) < 0)
+		err_sys("poll error");
+
+	return(n);
+}
+#endif
+
+ssize_t
+Recv(int fd, void *ptr, size_t nbytes, int flags)
+{
+	ssize_t		n;
+
+	if ( (n = recv(fd, ptr, nbytes, flags)) < 0)
+		err_sys("recv error");
+	return(n);
+}
+
+ssize_t
+Recvfrom(int fd, void *ptr, size_t nbytes, int flags,
+		 struct sockaddr *sa, socklen_t *salenptr)
+{
+	ssize_t		n;
+
+	if ( (n = recvfrom(fd, ptr, nbytes, flags, sa, salenptr)) < 0)
+		err_sys("recvfrom error");
+	return(n);
+}
+
+ssize_t
+Recvmsg(int fd, struct msghdr *msg, int flags)
+{
+	ssize_t		n;
+
+	if ( (n = recvmsg(fd, msg, flags)) < 0)
+		err_sys("recvmsg error");
+	return(n);
+}
+
+int
+Sockatmark(int fd)
+{
+	int		n;
+
+	if ( (n = sockatmark(fd)) < 0)
+		err_sys("sockatmark error");
+	return(n);
+}
+
+void
+Socketpair(int family, int type, int protocol, int *fd)
+{
+	int		n;
+
+	if ( (n = socketpair(family, type, protocol, fd)) < 0)
+		err_sys("socketpair error");
+}
+
+void
+sig_chld(int signo)
+{
+	pid_t	pid;
+	int		stat;
+
+	while ( ( pid = waitpid( -1, &stat, WNOHANG)) > 0) {
+		printf( "child %d terminated\n", pid);
+	}
+	return;
+}
+
+void
+str_cli( FILE *fp, int sockfd)
+{
+	int			maxfdp1, stdineof;
+	fd_set		rset;
+	char		buf[MAXLINE];
+	int		n;
+
+	stdineof = 0;
+	FD_ZERO( &rset);
+	for ( ; ; ) {
+		if ( stdineof == 0)
+			FD_SET( fileno( fp), &rset);    // subscribe to info for fp
+		FD_SET( sockfd, &rset);                 // subscribe to info for socket
+		maxfdp1 = max( fileno( fp), sockfd) + 1;
+		Select( maxfdp1, &rset, NULL, NULL, NULL);
+
+		if ( FD_ISSET( sockfd, &rset)) {	/* socket is readable */
+			if ( ( n = Read( sockfd, buf, MAXLINE)) == 0) {
+				if ( stdineof == 1)
+					return;		/* normal termination */
+				else
+					err_quit( "str_cli: server terminated prematurely");
+			}
+
+			Write( fileno( stdout), buf, n);
+		}
+
+		if ( FD_ISSET( fileno( fp), &rset)) {  /* input is readable */
+			if ( ( n = Read( fileno( fp), buf, MAXLINE)) == 0) {
+				stdineof = 1;
+				Shutdown( sockfd, SHUT_WR);	/* send FIN */
+				FD_CLR( fileno(fp), &rset);    // unsubscribe from info for fp
+				continue;
+			}
+
+			Writen( sockfd, buf, n);
+		}
+	}
+}
+
+void
+str_echo(int sockfd)
+{
+	long		arg1, arg2;
+	ssize_t		n;
+	char		line[MAXLINE];
+
+	for ( ; ; ) {
+		if ( ( n = Read(sockfd, line, MAXLINE)) == 0)
+			return;		/* connection closed by other end */
+
+		n = strlen( line);
+		Writen( sockfd, line, n);
+	}
+}
+
+void
+sigInt( int signo)
+{
+    fprintf( stdout, "sig int catched");
+    fflush( stdout);
+    exit( 0);
+}
+
+static void
+sigIo(int sigIo)
+{
+    fprintf( stdout, "sig io catched");
+    fflush( stdout);
+}
+
+/*
+ * 
+ */
+int
+main(int argc, char **argv)
+{
+            sigset_t set;
+            sigemptyset(&set);
+            sigaddset(&set, SIGIO);
+            sigprocmask(SIG_UNBLOCK, &set, NULL);
+            
+        Signal( SIGINT, sigInt);
+
+	int				listenfd, connfd;
+	pid_t				childpid;
+	socklen_t			clilen;
+	struct sockaddr_in              cliaddr, servaddr;
+
+	listenfd = Socket( AF_INET, SOCK_STREAM, 0);
+
+        int flags;
+        int pid = getpid();
+        /* Set a socket as nonblocking */
+        if ( ( flags = fcntl( listenfd, F_GETFL, 0)) < 0)
+            err_sys("F_GETFL error");
+        flags |= FASYNC;// O_NONBLOCK;
+        if ( fcntl( listenfd, F_SETFL, flags) < 0)
+            err_sys("F_SETFL error");
+        
+                                /*
+                         * catch SIGIO
+                         */
+                        struct sigaction	act, oact;
+
+                        act.sa_handler = sigIo;
+                        sigemptyset( &act.sa_mask);
+                        act.sa_flags = 0;
+                        act.sa_flags |= SA_RESTART;
+                        
+                        if ( sigaction( SIGIO, &act, &oact) < 0)
+                                err_sys(" SIGIO err");
+                        
+                        /*
+                        * Establish ownership:
+                        */
+                        if( fcntl( listenfd, F_SETOWN, pid) < 0)
+                            err_sys( "F_SETOWN error");
+
+	bzero( &servaddr, sizeof( servaddr));
+	servaddr.sin_family      = AF_INET;
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	servaddr.sin_port        = htons(SERV_PORT);
+
+        /* SO_REUSEADDR allows a new server to be started
+         * on the same port as an existing server that is
+         * bound to the wildcard address, as long as each
+         * instance binds a different local IP address.
+         * This is common for a site hosting multiple HTTP
+         * servers using the IP alias technique */
+        int reuseaddr_on = 1;
+        if( setsockopt( listenfd, SOL_SOCKET, SO_REUSEADDR,
+                &reuseaddr_on, sizeof( reuseaddr_on)) < 0)
+        {
+            // log
+        }
+
+	Bind( listenfd, (SA *) &servaddr, sizeof( servaddr));
+        
+	Listen( listenfd, LISTENQ);
+
+	Signal( SIGCHLD, sig_chld);
+
+	for ( ; ; ) {
+		clilen = sizeof(cliaddr);
+		if ( ( connfd = accept( listenfd, (SA *) &cliaddr, &clilen)) < 0) {
+			if ( errno == EINTR)
+				continue;		/* back to for() */
+			else
+				continue;//err_sys( "accept error");
+		}
+
+		if ( ( childpid = Fork()) == 0) {	/* child process */
+                    
+			Close( listenfd);               /* close listening socket */
+			str_echo( connfd);              /* process the request */
+			exit( 0);
+		}
+		Close( connfd);			/* parent closes connected socket */
+	}
+}
+
