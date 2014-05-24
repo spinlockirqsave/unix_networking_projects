16b960ec7bb9354b94fc080585e02e7cd6ba2e30
cf16 <cf16rocket@gmail.com>
2014-05-24 14:58:15 +0200

    protocol dependent and independent inet_ntop

---
+++ inet_ntop_sock_ntop/main.cpp
@@ -0,0 +1,437 @@
+/* 
+ * File:   main.cpp
+ * Author: piter cf16 eu
+ *
+ * Created on May 24, 2014, 2:42 PM
+ */
+
+#include <stdio.h>
+#include <errno.h>
+#include	<stdarg.h>		/* ANSI C header file */
+#include	<syslog.h>		/* for syslog() */
+#include <string.h>
+#include <stdlib.h>
+
+#include <unistd.h>
+#include	<sys/types.h>
+#include	<sys/socket.h>
+#include	<netinet/in.h>
+#include	<arpa/inet.h>
+
+#include <sys/resource.h>
+
+#define MAXLINE 4096
+#define SERV_PORT 9877
+#define SA struct sockaddr
+int		daemon_proc;		/* set nonzero by daemon_init() */
+
+/* Print message and return to caller
+ * Caller specifies "errnoflag" and "level" */
+static void
+err_doit(int errnoflag, int level, const char *fmt, va_list ap)
+{
+	int		errno_save, n;
+	char	buf[MAXLINE + 1];
+
+	errno_save = errno;		/* value caller might want printed */
+#ifdef	HAVE_VSNPRINTF
+	vsnprintf(buf, MAXLINE, fmt, ap);	/* safe */
+#else
+	vsprintf(buf, fmt, ap);					/* not safe */
+#endif
+	n = strlen(buf);
+	if (errnoflag)
+		snprintf(buf + n, MAXLINE - n, ": %s", strerror(errno_save));
+	strcat(buf, "\n");
+
+	if (daemon_proc) {
+		syslog(level, buf);
+	} else {
+		fflush(stdout);		/* in case stdout and stderr are the same */
+		fputs(buf, stderr);
+		fflush(stderr);
+	}
+	return;
+}
+
+/* Fatal error unrelated to system call
+ * Print message and terminate */
+
+void
+err_quit(const char *fmt, ...)
+{
+	va_list		ap;
+
+	va_start(ap, fmt);
+	err_doit(0, LOG_ERR, fmt, ap);
+	va_end(ap);
+	exit(1);
+}
+
+/* Fatal error related to system call
+ * Print message and terminate */
+
+void
+err_sys(const char *fmt, ...)
+{
+	va_list		ap;
+
+	va_start(ap, fmt);
+	err_doit(1, LOG_ERR, fmt, ap);
+	va_end(ap);
+	exit(1);
+}
+
+void
+Inet_pton(int family, const char *strptr, void *addrptr)
+{
+	int		n;
+
+	if ( (n = inet_pton(family, strptr, addrptr)) < 0)
+		err_sys("inet_pton error for %s", strptr);	/* errno set */
+	else if (n == 0)
+		err_quit("inet_pton error for %s", strptr);	/* errno not set */
+
+	/* nothing to return */
+}
+
+int
+Socket(int family, int type, int protocol)
+{
+	int		n;
+
+	if ( (n = socket(family, type, protocol)) < 0)
+		err_sys("socket error");
+	return(n);
+}
+
+void
+Connect(int fd, const struct sockaddr *sa, socklen_t salen)
+{
+	if (connect(fd, sa, salen) < 0)
+		err_sys("connect error");
+}
+
+ssize_t
+Read(int fd, void *ptr, size_t nbytes)
+{
+	ssize_t		n;
+
+	if ( (n = read(fd, ptr, nbytes)) == -1)
+		err_sys("read error");
+	return(n);
+}
+
+void
+Write(int fd, void *ptr, size_t nbytes)
+{
+	if (write(fd, ptr, nbytes) != nbytes)
+		err_sys("write error");
+}
+
+ssize_t						/* Write "n" bytes to a descriptor. */
+writen(int fd, const void *vptr, size_t n)
+{
+	size_t		nleft;
+	ssize_t		nwritten;
+	const char	*ptr;
+
+	ptr = ( const char*) vptr;
+	nleft = n;
+	while (nleft > 0) {
+		if ( (nwritten = write(fd, ptr, nleft)) <= 0) {
+			if (nwritten < 0 && errno == EINTR)
+				nwritten = 0;		/* and call write() again */
+			else
+				return(-1);			/* error */
+		}
+
+		nleft -= nwritten;
+		ptr   += nwritten;
+	}
+	return(n);
+}
+/* end writen */
+
+void
+Writen(int fd, void *ptr, size_t nbytes)
+{
+	if (writen(fd, ptr, nbytes) != nbytes)
+		err_sys("writen error");
+}
+
+int
+Select(int nfds, fd_set *readfds, fd_set *writefds, fd_set *exceptfds,
+       struct timeval *timeout)
+{
+	int		n;
+
+	if ( (n = select(nfds, readfds, writefds, exceptfds, timeout)) < 0)
+		err_sys("select error");
+	return(n);		/* can return 0 on timeout */
+}
+
+void
+Send(int fd, const void *ptr, size_t nbytes, int flags)
+{
+	if (send(fd, ptr, nbytes, flags) != (ssize_t)nbytes)
+		err_sys("send error");
+}
+
+void
+Sendto(int fd, const void *ptr, size_t nbytes, int flags,
+	   const struct sockaddr *sa, socklen_t salen)
+{
+	if (sendto(fd, ptr, nbytes, flags, sa, salen) != (ssize_t)nbytes)
+		err_sys("sendto error");
+}
+
+void
+Sendmsg(int fd, const struct msghdr *msg, int flags)
+{
+	unsigned int	i;
+	ssize_t			nbytes;
+
+	nbytes = 0;	/* must first figure out what return value should be */
+	for (i = 0; i < msg->msg_iovlen; i++)
+		nbytes += msg->msg_iov[i].iov_len;
+
+	if (sendmsg(fd, msg, flags) != nbytes)
+		err_sys("sendmsg error");
+}
+
+void
+Setsockopt(int fd, int level, int optname, const void *optval, socklen_t optlen)
+{
+	if (setsockopt(fd, level, optname, optval, optlen) < 0)
+		err_sys("setsockopt error");
+}
+
+void
+Shutdown(int fd, int how)
+{
+	if (shutdown(fd, how) < 0)
+		err_sys("shutdown error");
+}
+
+void
+Fclose(FILE *fp)
+{
+	if (fclose(fp) != 0)
+		err_sys("fclose error");
+}
+
+FILE *
+Fdopen(int fd, const char *type)
+{
+	FILE	*fp;
+
+	if ( (fp = fdopen(fd, type)) == NULL)
+		err_sys("fdopen error");
+
+	return(fp);
+}
+
+char *
+Fgets(char *ptr, int n, FILE *stream)
+{
+	char	*rptr;
+
+	if ( (rptr = fgets(ptr, n, stream)) == NULL && ferror(stream))
+		err_sys("fgets error");
+
+	return (rptr);
+}
+
+FILE *
+Fopen( const char *filename, const char *mode)
+{
+	FILE	*fp;
+
+	if ( ( fp = fopen( filename, mode)) == NULL)
+		err_sys( "fopen error");
+
+	return( fp);
+}
+
+void
+Fputs( const char *ptr, FILE *stream)
+{
+	if ( fputs( ptr, stream) == EOF)
+		err_sys( "fputs error");
+}
+
+ssize_t
+Recvfrom( int fd, void *ptr, size_t nbytes, int flags,
+		 struct sockaddr *sa, socklen_t *salenptr)
+{
+	ssize_t		n;
+
+	if ( ( n = recvfrom( fd, ptr, nbytes, flags, sa, salenptr)) < 0)
+		err_sys( "recvfrom error");
+	return(n);
+}
+
+/* inet_ntop protocol independent */
+#include <sys/un.h>
+
+char *
+sock_ntop( const struct sockaddr *sa, socklen_t salen)
+{
+    char		portstr[8];
+    static char         str[128];		/* Unix domain is largest */
+
+	switch ( sa->sa_family) {
+	case AF_INET: {
+		struct sockaddr_in	*sin = (struct sockaddr_in *) sa;
+
+		if ( inet_ntop(AF_INET, &sin->sin_addr, str, sizeof(str)) == NULL)
+			return(NULL);
+		if (ntohs(sin->sin_port) != 0) {
+			snprintf(portstr, sizeof(portstr), ":%d", ntohs(sin->sin_port));
+			strcat(str, portstr);
+		}
+		return(str);
+	}
+/* end sock_ntop */
+
+#ifdef	IPV6
+	case AF_INET6: {
+		struct sockaddr_in6	*sin6 = (struct sockaddr_in6 *) sa;
+
+		str[0] = '[';
+		if (inet_ntop(AF_INET6, &sin6->sin6_addr, str + 1, sizeof(str) - 1) == NULL)
+			return(NULL);
+		if (ntohs(sin6->sin6_port) != 0) {
+			snprintf(portstr, sizeof(portstr), "]:%d", ntohs(sin6->sin6_port));
+			strcat(str, portstr);
+			return(str);
+		}
+		return (str + 1);
+	}
+#endif
+
+#ifdef	AF_UNIX
+	case AF_UNIX: {
+		struct sockaddr_un	*unp = (struct sockaddr_un *) sa;
+
+			/* OK to have no pathname bound to the socket: happens on
+			   every connect() unless client calls bind() first. */
+		if (unp->sun_path[0] == 0)
+			strcpy(str, "(no pathname bound)");
+		else
+			snprintf(str, sizeof(str), "%s", unp->sun_path);
+		return(str);
+	}
+#endif
+
+#ifdef	HAVE_SOCKADDR_DL_STRUCT
+	case AF_LINK: {
+		struct sockaddr_dl	*sdl = (struct sockaddr_dl *) sa;
+
+		if (sdl->sdl_nlen > 0)
+			snprintf(str, sizeof(str), "%*s (index %d)",
+					 sdl->sdl_nlen, &sdl->sdl_data[0], sdl->sdl_index);
+		else
+			snprintf(str, sizeof(str), "AF_LINK, index=%d", sdl->sdl_index);
+		return(str);
+	}
+#endif
+	default:
+		snprintf(str, sizeof(str), "sock_ntop: unknown AF_xxx: %d, len %d",
+				 sa->sa_family, salen);
+		return(str);
+	}
+    return (NULL);
+}
+
+char *
+Sock_ntop(const struct sockaddr *sa, socklen_t salen)
+{
+	char	*ptr;
+
+	if ( ( ptr = sock_ntop(sa, salen)) == NULL)
+		err_sys("sock_ntop error");	/* inet_ntop() sets errno */
+	return(ptr);
+}
+
+void
+dg_cli( FILE *fp, int sockfd, const SA *pservaddr, socklen_t servlen)
+{
+	int	n;
+	char	sendline[MAXLINE], recvline[MAXLINE + 1];
+
+	while ( Fgets( sendline, MAXLINE, fp) != NULL) {
+
+		Sendto( sockfd, sendline, strlen(sendline), 0, pservaddr, servlen);
+
+		n = Recvfrom( sockfd, recvline, MAXLINE, 0, NULL, NULL);
+
+		recvline[n] = 0;	/* null terminate */
+		Fputs( recvline, stdout);
+	}
+}
+
+void
+dg_echo( int sockfd, SA *pcliaddr, socklen_t clilen)
+{
+	int			n;
+	socklen_t	len;
+	char		mesg[MAXLINE];
+
+	for ( ; ; ) {
+		len = clilen;
+		n = Recvfrom( sockfd, mesg, MAXLINE, 0, pcliaddr, &len);
+
+                /* print address protocol dependent */
+                char buff[100];
+                char portstr[8];
+                inet_ntop( AF_INET, &( (sockaddr_in* ) pcliaddr)->sin_addr, buff, 100);
+                sockaddr_in* sin = (sockaddr_in* ) pcliaddr;
+                if ( ntohs( sin->sin_port) != 0) {
+			snprintf( portstr, sizeof(portstr), ":%d", ntohs( sin->sin_port));
+			strcat( buff, portstr);
+		}
+                fprintf( stdout, "From:%s\n", buff);
+                
+                /* print protocol independent and with port number*/
+                fprintf( stdout, "From:%s\n", Sock_ntop( pcliaddr, len));
+                
+		Sendto( sockfd, mesg, n, 0, pcliaddr, len);
+	}
+}
+
+void
+Bind( int fd, const struct sockaddr *sa, socklen_t salen)
+{
+	if ( bind( fd, sa, salen) < 0)
+		err_sys( "bind error");
+}
+
+void
+Getpeername( int fd, struct sockaddr *sa, socklen_t *salenptr)
+{
+	if ( getpeername( fd, sa, salenptr) < 0)
+		err_sys( "getpeername error");
+}
+
+/*
+ * 
+ */
+int
+main(int argc, char **argv)
+{
+	int					sockfd;
+	struct sockaddr_in	servaddr, cliaddr;
+
+	sockfd = Socket( AF_INET, SOCK_DGRAM, 0);
+
+	bzero( &servaddr, sizeof(servaddr));
+	servaddr.sin_family      = AF_INET;
+	servaddr.sin_addr.s_addr = htonl( INADDR_ANY);
+	servaddr.sin_port        = htons( SERV_PORT);
+
+	Bind( sockfd, (SA *) &servaddr, sizeof( servaddr));
+
+	dg_echo( sockfd, (SA *) &cliaddr, sizeof( cliaddr));
+}
