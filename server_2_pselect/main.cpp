8dfc62c59d7c7eb5981629f6c8b7afd578d6563d
cf16 <cf16rocket@gmail.com>
2014-05-26 19:34:34 +0200

    setsockopt SO_REUSEADDR to allow for rebind

--- server_2_pselect/main.cpp
+++ server_2_pselect/main.cpp
@@ -561,9 +561,9 @@ main(int argc, char **argv)
 	socklen_t		clilen;
 	struct sockaddr_in	cliaddr, servaddr;
 
-	listenfd = Socket(AF_INET, SOCK_STREAM, 0);
+	listenfd = Socket( AF_INET, SOCK_STREAM, 0);
 
-	bzero(&servaddr, sizeof(servaddr));
+	bzero( &servaddr, sizeof( servaddr));
 	servaddr.sin_family      = AF_INET;
 	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
 	servaddr.sin_port        = htons(SERV_PORT);
