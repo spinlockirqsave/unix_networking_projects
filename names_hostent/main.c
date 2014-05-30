5aebe70e46e2bec081dd02c0178441fa097688ec
cf16 <cf16rocket@gmail.com>
2014-05-30 16:38:37 +0200

    gethostbyname, Inet_ntop

--- names_hostent/main.c
+++ names_hostent/main.c
@@ -644,24 +644,24 @@ main(int argc, char **argv)
 	char			str[INET_ADDRSTRLEN];
 	struct hostent	*hptr;
 
-	while (--argc > 0) {
+	while ( --argc > 0) {
 		ptr = *++argv;
-		if ( (hptr = gethostbyname(ptr)) == NULL) {
+		if ( ( hptr = gethostbyname(ptr)) == NULL) {
 			err_msg("gethostbyname error for host: %s: %s",
-					ptr, hstrerror(h_errno));
+					ptr, hstrerror( h_errno));
 			continue;
 		}
 		printf("official hostname: %s\n", hptr->h_name);
 
-		for (pptr = hptr->h_aliases; *pptr != NULL; pptr++)
-			printf("\talias: %s\n", *pptr);
+		for ( pptr = hptr->h_aliases; *pptr != NULL; pptr++)
+			printf( "\talias: %s\n", *pptr);
 
-		switch (hptr->h_addrtype) {
+		switch ( hptr->h_addrtype) {
 		case AF_INET:
 			pptr = hptr->h_addr_list;
 			for ( ; *pptr != NULL; pptr++)
 				printf("\taddress: %s\n",
-					Inet_ntop(hptr->h_addrtype, *pptr, str, sizeof(str)));
+					Inet_ntop( hptr->h_addrtype, *pptr, str, sizeof(str)));
 			break;
 
 		default:
