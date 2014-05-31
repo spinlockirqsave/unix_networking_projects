3db829ea076a32145ae2f69feea32a99502a1a70
cf16 <cf16rocket@gmail.com>
2014-05-31 15:54:02 +0200

    gethostbyname, Inet_ntop, gethostbyaddr

---
+++ names_hostent2/main.c
@@ -0,0 +1,693 @@
+/* 
+ * File:   main.c
+ * Author: piter cf16 eu
+ *
+ * Created on May 30, 2014, 5:03 PM
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/* All data returned by the network data base library are supplied in
+   host order and returned in network order (suitable for use in
+   system calls).  */
+#include <netdb.h>
+
+#include <errno.h>
+#include	<stdarg.h>		/* ANSI C header file */
+#include	<syslog.h>		/* for syslog() */
+#include <string.h>
+#include <stdlib.h>
+
+#include <unistd.h>
+#include	<sys/types.h>
+#include        <sys/wait.h>            /* for waitpid */
+#include	<sys/socket.h>
+#include	<netinet/in.h>
+#include	<arpa/inet.h>
+
+#include <math.h>
+
+#include <fcntl.h>
+#include <poll.h>
+
+#include <limits.h>		/* for open_max() */
+
+/* POSIX requires that an #include of <poll.h> DefinE INFTIM, but many
+   systems still DefinE it in <sys/stropts.h>.  We don't want to include
+   all the STREAMS stuff if it's not needed, so we just DefinE INFTIM here.
+   This is the standard value, but there's no guarantee it is -1. */
+
+#define INFTIM          (-1)    /* infinite poll timeout */
+#define	HAVE_POLL
+
+#ifdef  OPEN_MAX
+static long openmax = open_max();
+#else
+static long openmax = 0;
+#endif
+
+/*
+ * If open_max() is indeterminate, we're not
+ * guaranteed that this is adequate.
+ */
+#define OPEN_MAX_GUESS  256
+
+void
+err_sys(const char *fmt, ...);
+
+long
+open_max(void)
+{
+    if ( openmax == 0) {     /* first time through */
+        errno = 0;
+        if (( openmax = sysconf( _SC_OPEN_MAX)) < 0) {
+            if ( errno == 0)
+                openmax = OPEN_MAX_GUESS;   /* it's indeterminate */
+            else
+                err_sys( "sysconf error for _SC_open_max()");
+        }
+    }
+
+    return(openmax);
+}
+
+#define MAXLINE 4096
+#define SERV_PORT 9877
+#define SA struct sockaddr
+int		daemon_proc;		/* set nonzero by daemon_init() */
+
+#define	min(a,b)	((a) < (b) ? (a) : (b))
+#define	max(a,b)	((a) > (b) ? (a) : (b))
+
+#define	LISTENQ		1024	/* 2nd argument to listen() */
+
+typedef void (Sigfunc)(int);    /* signal handler */
+static void	err_doit(int, int, const char *, va_list);
+
+/* Nonfatal error related to system call
+ * Print message and return */
+void
+err_ret(const char *fmt, ...)
+{
+	va_list		ap;
+
+	va_start(ap, fmt);
+	err_doit(1, LOG_INFO, fmt, ap);
+	va_end(ap);
+	return;
+}
+
+/* Nonfatal error unrelated to system call
+ * Print message and return */
+void
+err_msg(const char *fmt, ...)
+{
+	va_list		ap;
+
+	va_start(ap, fmt);
+	err_doit(0, LOG_INFO, fmt, ap);
+	va_end(ap);
+	return;
+}
+
+/* Print message and return to caller
+ * Caller specifies "errnoflag" and "level" */
+static void
+err_doit( int errnoflag, int level, const char *fmt, va_list ap)
+{
+	int		errno_save, n;
+	char	buf[MAXLINE + 1];
+
+	errno_save = errno;		/* value caller might want printed */
+#ifdef	HAVE_VSNPRINTF
+	vsnprintf(buf, MAXLINE, fmt, ap);	/* safe */
+#else
+	vsprintf(buf, fmt, ap);					/* not safe */
+#endif
+	n = strlen(buf);
+	if (errnoflag)
+		snprintf(buf + n, MAXLINE - n, ": %s", strerror(errno_save));
+	strcat(buf, "\n");
+
+	if (daemon_proc) {
+		syslog(level, buf);
+	} else {
+		fflush(stdout);		/* in case stdout and stderr are the same */
+		fputs(buf, stderr);
+		fflush(stderr);
+	}
+	return;
+}
+
+/* Fatal error unrelated to system call
+ * Print message and terminate */
+
+void
+err_quit(const char *fmt, ...)
+{
+	va_list		ap;
+
+	va_start(ap, fmt);
+	err_doit(0, LOG_ERR, fmt, ap);
+	va_end(ap);
+	exit(1);
+}
+
+/* Fatal error related to system call
+ * Print message and terminate */
+
+void
+err_sys(const char *fmt, ...)
+{
+	va_list		ap;
+
+	va_start(ap, fmt);
+	err_doit(1, LOG_ERR, fmt, ap);
+	va_end(ap);
+	exit(1);
+}
+
+Sigfunc *
+signal(int signo, Sigfunc *func)
+{
+	struct sigaction	act, oact;
+
+	act.sa_handler = func;
+	sigemptyset(&act.sa_mask);
+	act.sa_flags = 0;
+	if (signo == SIGALRM) {
+#ifdef	SA_INTERRUPT
+		act.sa_flags |= SA_INTERRUPT;	/* SunOS 4.x */
+#endif
+	} else {
+#ifdef	SA_RESTART
+		act.sa_flags |= SA_RESTART;		/* SVR4, 44BSD */
+#endif
+	}
+	if (sigaction(signo, &act, &oact) < 0)
+		return(SIG_ERR);
+	return(oact.sa_handler);
+}
+/* end signal */
+
+Sigfunc *
+Signal(int signo, Sigfunc *func)	/* for our signal() function */
+{
+	Sigfunc	*sigfunc;
+
+	if ( (sigfunc = signal(signo, func)) == SIG_ERR)
+		err_sys("signal error");
+	return(sigfunc);
+}
+
+int
+Socket(int family, int type, int protocol)
+{
+	int		n;
+
+	if ( (n = socket(family, type, protocol)) < 0)
+		err_sys("socket error");
+	return(n);
+}
+
+void
+Connect(int fd, const struct sockaddr *sa, socklen_t salen)
+{
+	if (connect(fd, sa, salen) < 0)
+		err_sys("connect error");
+}
+
+ssize_t
+Read(int fd, void *ptr, size_t nbytes)
+{
+	ssize_t		n;
+
+	if ( (n = read(fd, ptr, nbytes)) == -1)
+		err_sys("read error");
+	return(n);
+}
+
+void
+Write(int fd, void *ptr, size_t nbytes)
+{
+	if (write(fd, ptr, nbytes) != nbytes)
+		err_sys("write error");
+}
+
+ssize_t						/* Write "n" bytes to a descriptor. */
+writen(int fd, const void *vptr, size_t n)
+{
+	size_t		nleft;
+	ssize_t		nwritten;
+	const char	*ptr;
+
+	ptr = ( const char*) vptr;
+	nleft = n;
+	while (nleft > 0) {
+		if ( (nwritten = write(fd, ptr, nleft)) <= 0) {
+			if (nwritten < 0 && errno == EINTR)
+				nwritten = 0;		/* and call write() again */
+			else
+				return(-1);			/* error */
+		}
+
+		nleft -= nwritten;
+		ptr   += nwritten;
+	}
+	return(n);
+}
+/* end writen */
+
+void
+Writen(int fd, void *ptr, size_t nbytes)
+{
+	if (writen(fd, ptr, nbytes) != nbytes)
+		err_sys("writen error");
+}
+
+int
+Select(int nfds, fd_set *readfds, fd_set *writefds, fd_set *exceptfds,
+       struct timeval *timeout)
+{
+	int		n;
+
+	if ( (n = select(nfds, readfds, writefds, exceptfds, timeout)) < 0)
+		err_sys("select error");
+	return(n);		/* can return 0 on timeout */
+}
+
+void
+Send(int fd, const void *ptr, size_t nbytes, int flags)
+{
+	if (send(fd, ptr, nbytes, flags) != (ssize_t)nbytes)
+		err_sys("send error");
+}
+
+void
+Sendto(int fd, const void *ptr, size_t nbytes, int flags,
+	   const struct sockaddr *sa, socklen_t salen)
+{
+	if (sendto(fd, ptr, nbytes, flags, sa, salen) != (ssize_t)nbytes)
+		err_sys("sendto error");
+}
+
+void
+Sendmsg(int fd, const struct msghdr *msg, int flags)
+{
+	unsigned int	i;
+	ssize_t			nbytes;
+
+	nbytes = 0;	/* must first figure out what return value should be */
+	for (i = 0; i < msg->msg_iovlen; i++)
+		nbytes += msg->msg_iov[i].iov_len;
+
+	if (sendmsg(fd, msg, flags) != nbytes)
+		err_sys("sendmsg error");
+}
+
+void
+Setsockopt(int fd, int level, int optname, const void *optval, socklen_t optlen)
+{
+	if (setsockopt(fd, level, optname, optval, optlen) < 0)
+		err_sys("setsockopt error");
+}
+
+void
+Shutdown(int fd, int how)
+{
+	if (shutdown(fd, how) < 0)
+		err_sys("shutdown error");
+}
+
+char *
+Fgets(char *ptr, int n, FILE *stream)
+{
+	char	*rptr;
+
+	if ( (rptr = fgets(ptr, n, stream)) == NULL && ferror(stream))
+		err_sys("fgets error");
+
+	return (rptr);
+}
+
+FILE *
+Fopen(const char *filename, const char *mode)
+{
+	FILE	*fp;
+
+	if ( (fp = fopen(filename, mode)) == NULL)
+		err_sys("fopen error");
+
+	return(fp);
+}
+
+void
+Fputs(const char *ptr, FILE *stream)
+{
+	if (fputs(ptr, stream) == EOF)
+		err_sys("fputs error");
+}
+
+int
+Accept(int fd, struct sockaddr *sa, socklen_t *salenptr)
+{
+	int		n;
+
+again:
+	if ( (n = accept(fd, sa, salenptr)) < 0) {
+#ifdef	EPROTO
+		if (errno == EPROTO || errno == ECONNABORTED)
+#else
+		if (errno == ECONNABORTED)
+#endif
+			goto again;
+		else
+			err_sys("accept error");
+	}
+	return(n);
+}
+
+void
+Bind(int fd, const struct sockaddr *sa, socklen_t salen)
+{
+	if (bind(fd, sa, salen) < 0)
+		err_sys("bind error");
+}
+
+void
+Close(int fd)
+{
+	if (close(fd) == -1)
+		err_sys("close error");
+}
+
+int
+Fcntl(int fd, int cmd, int arg)
+{
+	int	n;
+
+	if ( (n = fcntl(fd, cmd, arg)) == -1)
+		err_sys("fcntl error");
+	return(n);
+}
+
+pid_t
+Fork(void)
+{
+	pid_t	pid;
+
+	if ( (pid = fork()) == -1)
+		err_sys("fork error");
+	return(pid);
+}
+
+void
+Getpeername(int fd, struct sockaddr *sa, socklen_t *salenptr)
+{
+	if (getpeername(fd, sa, salenptr) < 0)
+		err_sys("getpeername error");
+}
+
+void
+Getsockname(int fd, struct sockaddr *sa, socklen_t *salenptr)
+{
+	if (getsockname(fd, sa, salenptr) < 0)
+		err_sys("getsockname error");
+}
+
+void
+Getsockopt(int fd, int level, int optname, void *optval, socklen_t *optlenptr)
+{
+	if (getsockopt(fd, level, optname, optval, optlenptr) < 0)
+		err_sys("getsockopt error");
+}
+
+/* include Listen */
+void
+Listen(int fd, int backlog)
+{
+	char	*ptr;
+
+		/*4can override 2nd argument with environment variable */
+	if ( (ptr = getenv("LISTENQ")) != NULL)
+		backlog = atoi(ptr);
+
+	if (    listen(fd, backlog) < 0)
+		err_sys("listen error");
+}
+/* end Listen */
+
+#ifdef	HAVE_POLL
+int
+Poll(struct pollfd *fdarray, unsigned long nfds, int timeout)
+{
+	int		n;
+
+	if ( (n = poll(fdarray, nfds, timeout)) < 0)
+		err_sys("poll error");
+
+	return(n);
+}
+#endif
+
+ssize_t
+Recv(int fd, void *ptr, size_t nbytes, int flags)
+{
+	ssize_t		n;
+
+	if ( (n = recv(fd, ptr, nbytes, flags)) < 0)
+		err_sys("recv error");
+	return(n);
+}
+
+ssize_t
+Recvfrom(int fd, void *ptr, size_t nbytes, int flags,
+		 struct sockaddr *sa, socklen_t *salenptr)
+{
+	ssize_t		n;
+
+	if ( (n = recvfrom(fd, ptr, nbytes, flags, sa, salenptr)) < 0)
+		err_sys("recvfrom error");
+	return(n);
+}
+
+ssize_t
+Recvmsg(int fd, struct msghdr *msg, int flags)
+{
+	ssize_t		n;
+
+	if ( (n = recvmsg(fd, msg, flags)) < 0)
+		err_sys("recvmsg error");
+	return(n);
+}
+
+int
+Sockatmark(int fd)
+{
+	int		n;
+
+	if ( (n = sockatmark(fd)) < 0)
+		err_sys("sockatmark error");
+	return(n);
+}
+
+void
+Socketpair(int family, int type, int protocol, int *fd)
+{
+	int		n;
+
+	if ( (n = socketpair(family, type, protocol, fd)) < 0)
+		err_sys("socketpair error");
+}
+
+/*
+ * Return a string containing some additional information after a
+ * host name or address lookup error - gethostbyname() or gethostbyaddr().
+ *
+ * This is only compiled if the local host does not provide it--recent
+ * versions of BIND supply this function.
+ */
+const char *
+hstrerror(int err)
+{
+	if (err == 0)
+		return("no error");
+
+	if (err == HOST_NOT_FOUND)
+		return("Unknown host");
+
+	if (err == TRY_AGAIN)
+		return("Hostname lookup failure");
+
+	if (err == NO_RECOVERY)
+		return("Unknown server error");
+
+	if (err == NO_DATA)
+        return("No address associated with name");
+
+	return("unknown error");
+}
+
+/* inet_ntop protocol independent */
+#include <sys/un.h>
+
+char *
+sock_ntop( const struct sockaddr *sa, socklen_t salen)
+{
+    char		portstr[8];
+    static char         str[128];		/* Unix domain is largest */
+
+	switch ( sa->sa_family) {
+	case AF_INET: {
+		struct sockaddr_in	*sin = (struct sockaddr_in *) sa;
+
+		if ( inet_ntop(AF_INET, &sin->sin_addr, str, sizeof(str)) == NULL)
+			return(NULL);
+		if (ntohs(sin->sin_port) != 0) {
+			snprintf(portstr, sizeof(portstr), ":%d", ntohs(sin->sin_port));
+			strcat(str, portstr);
+		}
+		return(str);
+	}
+/* end sock_ntop */
+
+#ifdef	IPV6
+	case AF_INET6: {
+		struct sockaddr_in6	*sin6 = (struct sockaddr_in6 *) sa;
+
+		str[0] = '[';
+		if (inet_ntop(AF_INET6, &sin6->sin6_addr, str + 1, sizeof(str) - 1) == NULL)
+			return(NULL);
+		if (ntohs(sin6->sin6_port) != 0) {
+			snprintf(portstr, sizeof(portstr), "]:%d", ntohs(sin6->sin6_port));
+			strcat(str, portstr);
+			return(str);
+		}
+		return (str + 1);
+	}
+#endif
+
+#ifdef	AF_UNIX
+	case AF_UNIX: {
+		struct sockaddr_un	*unp = (struct sockaddr_un *) sa;
+
+			/* OK to have no pathname bound to the socket: happens on
+			   every connect() unless client calls bind() first. */
+		if (unp->sun_path[0] == 0)
+			strcpy(str, "(no pathname bound)");
+		else
+			snprintf(str, sizeof(str), "%s", unp->sun_path);
+		return(str);
+	}
+#endif
+
+#ifdef	HAVE_SOCKADDR_DL_STRUCT
+	case AF_LINK: {
+		struct sockaddr_dl	*sdl = (struct sockaddr_dl *) sa;
+
+		if (sdl->sdl_nlen > 0)
+			snprintf(str, sizeof(str), "%*s (index %d)",
+					 sdl->sdl_nlen, &sdl->sdl_data[0], sdl->sdl_index);
+		else
+			snprintf(str, sizeof(str), "AF_LINK, index=%d", sdl->sdl_index);
+		return(str);
+	}
+#endif
+	default:
+		snprintf(str, sizeof(str), "sock_ntop: unknown AF_xxx: %d, len %d",
+				 sa->sa_family, salen);
+		return(str);
+	}
+    return (NULL);
+}
+
+char *
+Sock_ntop(const struct sockaddr *sa, socklen_t salen)
+{
+	char	*ptr;
+
+	if ( ( ptr = sock_ntop(sa, salen)) == NULL)
+		err_sys("sock_ntop error");	/* inet_ntop() sets errno */
+	return(ptr);
+}
+
+const char *
+Inet_ntop(int family, const void *addrptr, char *strptr, size_t len)
+{
+	const char	*ptr;
+
+	if (strptr == NULL)		/* check for old code */
+		err_quit( "NULL 3rd argument to inet_ntop");
+	if ( (ptr = inet_ntop( family, addrptr, strptr, len)) == NULL)
+		err_sys( "inet_ntop error");		/* sets errno */
+	return(ptr);
+}
+
+void
+Inet_pton(int family, const char *strptr, void *addrptr)
+{
+	int		n;
+
+	if ( (n = inet_pton( family, strptr, addrptr)) < 0)
+		err_sys( "inet_pton error for %s", strptr);	/* errno set */
+	else if ( n == 0)
+		err_quit( "inet_pton error for %s", strptr);	/* errno not set */
+
+	/* nothing to return */
+}
+
+/*
+ * 
+ */
+int
+main(int argc, char **argv)
+{
+	char			*ptr, **pptr;
+	char			str[INET_ADDRSTRLEN];
+	struct hostent	*hptr;
+
+	while (--argc > 0) {
+		ptr = *++argv;
+		if ( (hptr = gethostbyname(ptr)) == NULL) {
+			err_msg("gethostbyname error for host: %s: %s",
+					ptr, hstrerror(h_errno));
+			continue;
+		}
+		printf("official hostname: %s\n", hptr->h_name);
+
+		for (pptr = hptr->h_aliases; *pptr != NULL; pptr++)
+			printf("	alias: %s\n", *pptr);
+
+		switch (hptr->h_addrtype) {
+		case AF_INET:
+#ifdef	AF_INET6
+		case AF_INET6:
+#endif
+			pptr = hptr->h_addr_list;
+			for ( ; *pptr != NULL; pptr++) {
+				printf("\taddress: %s\n",
+					Inet_ntop(hptr->h_addrtype, *pptr, str, sizeof(str)));
+
+				if ( (hptr = gethostbyaddr(*pptr, hptr->h_length,
+										   hptr->h_addrtype)) == NULL) {
+					printf("\t(gethostbyaddr failed)\n");
+                                        break;
+                                }
+				else if (hptr->h_name != NULL)
+					printf("\tname = %s\n", hptr->h_name);
+				else
+					printf("\t(no hostname returned by gethostbyaddr)\n");
+			}
+			break;
+
+		default:
+			err_ret("unknown address type");
+			break;
+		}
+	}
+	exit(0);
+}
+
+
+
