d88b24ee04df1dc6bc66b4ea2c33b74350340002
cf16 <cf16rocket@gmail.com>
2014-06-08 18:49:26 +0200

    protocol dependent/independent inet_ntop Sock_ntop

--- server_1_udp/main.cpp
+++ server_1_udp/main.cpp
@@ -272,6 +272,89 @@ Recvfrom( int fd, void *ptr, size_t nbytes, int flags,
 	return(n);
 }
 
+/* inet_ntop protocol independent */
+#include <sys/un.h>
+
+char *
+sock_ntop( const struct sockaddr *sa, socklen_t salen)
+{
+    char		portstr[8];
+    static char         str[128];		/* Unix domain is largest */
+
+	switch ( sa->sa_family) {
+	case AF_INET: {
+		struct sockaddr_in	*sin = (struct sockaddr_in *) sa;
+
+		if ( inet_ntop(AF_INET, &sin->sin_addr, str, sizeof(str)) == NULL)
+			return(NULL);
+		if (ntohs(sin->sin_port) != 0) {
+			snprintf(portstr, sizeof(portstr), ":%d", ntohs(sin->sin_port));
+			strcat(str, portstr);
+		}
+		return(str);
+	}
+/* end sock_ntop */
+
+#ifdef	IPV6
+	case AF_INET6: {
+		struct sockaddr_in6	*sin6 = (struct sockaddr_in6 *) sa;
+
+		str[0] = '[';
+		if (inet_ntop(AF_INET6, &sin6->sin6_addr, str + 1, sizeof(str) - 1) == NULL)
+			return(NULL);
+		if (ntohs(sin6->sin6_port) != 0) {
+			snprintf(portstr, sizeof(portstr), "]:%d", ntohs(sin6->sin6_port));
+			strcat(str, portstr);
+			return(str);
+		}
+		return (str + 1);
+	}
+#endif
+
+#ifdef	AF_UNIX
+	case AF_UNIX: {
+		struct sockaddr_un	*unp = (struct sockaddr_un *) sa;
+
+			/* OK to have no pathname bound to the socket: happens on
+			   every connect() unless client calls bind() first. */
+		if (unp->sun_path[0] == 0)
+			strcpy(str, "(no pathname bound)");
+		else
+			snprintf(str, sizeof(str), "%s", unp->sun_path);
+		return(str);
+	}
+#endif
+
+#ifdef	HAVE_SOCKADDR_DL_STRUCT
+	case AF_LINK: {
+		struct sockaddr_dl	*sdl = (struct sockaddr_dl *) sa;
+
+		if (sdl->sdl_nlen > 0)
+			snprintf(str, sizeof(str), "%*s (index %d)",
+					 sdl->sdl_nlen, &sdl->sdl_data[0], sdl->sdl_index);
+		else
+			snprintf(str, sizeof(str), "AF_LINK, index=%d", sdl->sdl_index);
+		return(str);
+	}
+#endif
+	default:
+		snprintf(str, sizeof(str), "sock_ntop: unknown AF_xxx: %d, len %d",
+				 sa->sa_family, salen);
+		return(str);
+	}
+    return (NULL);
+}
+
+char *
+Sock_ntop(const struct sockaddr *sa, socklen_t salen)
+{
+	char	*ptr;
+
+	if ( ( ptr = sock_ntop(sa, salen)) == NULL)
+		err_sys("sock_ntop error");	/* inet_ntop() sets errno */
+	return(ptr);
+}
+
 void
 dg_cli( FILE *fp, int sockfd, const SA *pservaddr, socklen_t servlen)
 {
@@ -308,6 +391,16 @@ dg_echo( int sockfd, SA *pcliaddr, socklen_t clilen)
                 char buff[100];
                 inet_ntop( AF_INET, &( (sockaddr_in* ) pcliaddr)->sin_addr, buff, 100);
                 fprintf( stdout, "From:%s\n", buff);
+                char portstr[8];
+                sockaddr_in* sin = (sockaddr_in* ) pcliaddr;
+                if ( ntohs( sin->sin_port) != 0) {
+			snprintf( portstr, sizeof(portstr), ":%d", ntohs( sin->sin_port));
+			strcat( buff, portstr);
+		}
+                fprintf( stdout, "protocol dependent! From:%s\n", buff);
+                
+                /* 2. print protocol independent and with port number*/
+                fprintf( stdout, "protocol independent From:%s\n", Sock_ntop( pcliaddr, len));
                 
 		Sendto( sockfd, mesg, n, 0, pcliaddr, len);
 	}
@@ -347,4 +440,3 @@ main(int argc, char **argv)
 
 	dg_echo( sockfd, (SA *) &cliaddr, sizeof( cliaddr));
 }
-
