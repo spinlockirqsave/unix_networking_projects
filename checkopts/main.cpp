a9ead7df968e1a2571c1eec53cf8ea94fc7d53dd
cf16 <cf16rocket@gmail.com>
2014-06-16 16:43:07 +0200

    setsockopt SO_REUSEADDR to allow for rebind

--- checkopts/main.cpp
+++ checkopts/main.cpp
@@ -209,7 +209,7 @@ main(int argc, char **argv)
 	struct sock_opts	*ptr;
 
 	for (ptr = sock_opts; ptr->opt_str != NULL; ptr++) {
-		printf("%s: ", ptr->opt_str);
+		printf("TCP %s: ", ptr->opt_str);
 		if (ptr->opt_val_str == NULL)
 			printf("(undefined)\n");
 		else {
@@ -222,6 +222,42 @@ main(int argc, char **argv)
 #ifdef	IPV6
 			case IPPROTO_IPV6:
 				fd = Socket(AF_INET6, SOCK_STREAM, 0);
+				break;
+#endif
+#ifdef	IPPROTO_SCTP
+			case IPPROTO_SCTP:
+				fd = Socket(AF_INET, SOCK_SEQPACKET, IPPROTO_SCTP);
+				break;
+#endif
+			default:
+				err_quit("Can't create fd for level %d\n", ptr->opt_level);
+			}
+
+			len = sizeof(val);
+			if ( getsockopt( fd, ptr->opt_level, ptr->opt_name,
+						   &val, &len) == -1) {
+				err_ret( "getsockopt error");
+			} else {
+				printf( "default = %s\n", (*ptr->opt_val_str)(&val, len));
+			}
+			close(fd);
+		}
+	}
+
+        for (ptr = sock_opts; ptr->opt_str != NULL; ptr++) {
+		printf("UDP %s: ", ptr->opt_str);
+		if (ptr->opt_val_str == NULL)
+			printf("(undefined)\n");
+		else {
+			switch(ptr->opt_level) {
+			case SOL_SOCKET:
+			case IPPROTO_IP:
+			case IPPROTO_TCP:
+				fd = Socket(AF_INET, SOCK_DGRAM, 0);
+				break;
+#ifdef	IPV6
+			case IPPROTO_IPV6:
+				fd = Socket(AF_INET6, SOCK_DGRAM, 0);
 				break;
 #endif
 #ifdef	IPPROTO_SCTP
