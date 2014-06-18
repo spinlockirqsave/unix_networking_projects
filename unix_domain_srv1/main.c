08085f6632abd93d2247885bae3ae3bb4ae583f4
cf16 <cf16rocket@gmail.com>
2014-06-18 12:52:47 +0200

    Unix domain stream echo server

--- unix_domain_srv1/main.c
+++ unix_domain_srv1/main.c
@@ -34,8 +34,8 @@ sig_chld(int signo)
 	pid_t	pid;
 	int		stat;
 
-	while ( ( pid = waitpid(-1, &stat, WNOHANG)) > 0) {
-		printf("child %d terminated\n", pid);
+	while ( ( pid = waitpid( -1, &stat, WNOHANG)) > 0) {
+		printf( "child %d terminated\n", pid);
 	}
 	return;
 }
