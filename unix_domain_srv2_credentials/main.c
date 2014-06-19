65875696587c58b50818a94693c61245068ee485
cf16 <cf16rocket@gmail.com>
2014-06-20 00:46:25 +0200

    check credentials passed in msghdr

---
+++ unix_domain_srv2_credentials/main.c
@@ -0,0 +1,163 @@
+/* 
+ * File:   main.c
+ * Author: piter
+ *
+ * Created on June 19, 2014, 11:21 PM
+ * 
+ * A Unix domain socket or IPC socket (inter-process communication socket)
+ * is a data communications endpoint for exchanging data between processes
+ * executing within the same host operating system. While similar in
+ * functionality to named pipes, Unix domain sockets may be created
+ * as connection‑mode (SOCK_STREAM or SOCK_SEQPACKET) or as connectionless
+ * (SOCK_DGRAM), while pipes are streams only. Processes using Unix domain
+ * sockets do not need to share a common ancestry.
+ * 
+ * Unix domain sockets use the file system as their address name space.
+ * They are referenced by processes as inodes in the file system.
+ * This allows two processes to open the same socket in order to communicate.
+ * However, communication occurs entirely within the operating system kernel.
+ * In addition to sending data, processes may send file descriptors across
+ * a Unix domain socket connection using the sendmsg() and recvmsg() system
+ * calls.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include "networking_functions.h"
+
+//SCM_CREDENTIALS c;
+
+//typedef struct ucred  ucred;
+//typedef ucred cmsgcred;
+
+struct ucred
+{
+  pid_t pid;			/* PID of sending process.  */
+  uid_t uid;			/* UID of sending process.  */
+  gid_t gid;			/* GID of sending process.  */
+};
+
+#define	CONTROL_LEN (sizeof(struct cmsghdr) + sizeof( struct ucred))
+
+ssize_t
+read_cred( int fd, void *ptr, size_t nbytes, struct ucred * cmsgcredptr)
+{
+	struct msghdr	msg;
+	struct iovec	iov[1];
+	char    control[CONTROL_LEN];
+	int	n;
+
+	msg.msg_name = NULL;
+	msg.msg_namelen = 0;
+	iov[0].iov_base = ptr;
+	iov[0].iov_len = nbytes;
+	msg.msg_iov = iov;
+	msg.msg_iovlen = 1;
+	msg.msg_control = control;
+	msg.msg_controllen = sizeof(control);
+	msg.msg_flags = 0;
+
+	if ( ( n = recvmsg( fd, &msg, 0)) < 0)
+		return(n);
+
+	return(n);
+}
+
+
+void
+str_echo(int sockfd)
+{
+	ssize_t			n;
+	int			i;
+	char			buf[MAXLINE];
+	struct ucred    cred;
+
+again:
+	while ( ( n = read_cred( sockfd, buf, MAXLINE, &cred)) > 0) {
+		if (cred.uid == 0) {
+			printf("uuid==0\n");
+                        printf("PID of sender = %d\n", cred.pid);
+			printf("real user ID = %d\n", cred.uid);
+			printf("real group ID = %d\n", cred.gid);
+			printf("\n");
+		} else {
+			printf("PID of sender = %d\n", cred.pid);
+			printf("real user ID = %d\n", cred.uid);
+			printf("real group ID = %d\n", cred.gid);
+			printf("\n");
+		}
+		Writen(sockfd, buf, n);
+	}
+
+	if (n < 0 && errno == EINTR)
+		goto again;
+	else if (n < 0)
+		err_sys("str_echo: read error");
+}
+
+void
+sig_chld(int signo)
+{
+	pid_t	pid;
+	int		stat;
+
+	while ( ( pid = waitpid( -1, &stat, WNOHANG)) > 0) {
+		printf( "child %d terminated\n", pid);
+	}
+	return;
+}
+
+
+/*
+ * 
+ */
+int
+main( int argc, char **argv)
+{
+	int			listenfd, connfd, clilen;
+	socklen_t		len, childpid;
+	struct sockaddr_un	servaddr, addr2;
+        struct sockaddr_un	cliaddr;
+
+	if ( argc != 2)
+		err_quit( "usage: unix_domain_srv2_credentials <pathname>");
+
+        /* Create IPC socket */
+	listenfd = Socket( AF_LOCAL, SOCK_STREAM, 0);
+
+        /* unlink in case the link exists from an earlier run of the server */
+	unlink( argv[1]);		/* OK if this fails */
+
+	bzero( &servaddr, sizeof(servaddr));
+	servaddr.sun_family = AF_LOCAL;
+	strncpy( servaddr.sun_path, argv[1], sizeof(servaddr.sun_path)-1);
+	Bind( listenfd, (SA *) &servaddr, SUN_LEN(&servaddr));
+
+	len = sizeof( addr2);
+	Getsockname( listenfd, (SA *) &addr2, &len);
+	printf( "bound name = %s, returned len = %d\n", addr2.sun_path, len);
+	
+	Listen( listenfd, LISTENQ);
+
+	Signal( SIGCHLD, sig_chld);
+
+	for ( ; ; ) {
+		clilen = sizeof(cliaddr);
+		if ( ( connfd = accept( listenfd, (SA *) &cliaddr, &clilen)) < 0) {
+			if ( errno == EINTR)
+				continue;		/* back to for() */
+			else
+				err_sys( "accept error");
+		}
+
+		if ( ( childpid = Fork()) == 0) {	/* child process */
+			Close( listenfd);               /* close listening socket */
+			str_echo( connfd);              /* process request */
+			exit( 0);
+		}
+                
+		Close( connfd);                         /* parent closes connected socket */
+	}
+}
---
+++ unix_domain_srv2_credentials/read_fd.c
@@ -0,0 +1,99 @@
+
+#include "networking_functions.h"
+
+/* 
+ * The client and server must have some application protocol so that the receiver
+ * of the descriptor knows when to expect it. If the receiver calls recvmsg without
+ * allocating room to receive the descriptor, and a descriptor was passed and is
+ * ready to be read, the descriptor that was being passed is closed (p. 518 of TCPv2).
+ * Also, the MSG_PEEK flag should be avoided with recvmsg if a descriptor is expected,
+ * as the result is unpredictable.
+ */
+ssize_t
+read_fd( int fd, void *ptr, size_t nbytes, int *recvfd)
+{
+	struct msghdr	msg;
+	struct iovec	iov[1];
+	ssize_t		n;
+
+        /* 
+        * we do have msghdr.msg_control on Ubuntu. POSIX specifies that the descriptor be sent
+        * as ancillary data (the msg_control member of the msghdr structure, Section 14.6),
+        * but older implementations use the msg_accrights member.
+        */
+#ifdef	HAVE_MSGHDR_MSG_CONTROL
+        
+        /* helper */
+	union {
+	  struct cmsghdr	cm;
+	  char			control[CMSG_SPACE(sizeof(int))];
+	} control_un;
+        
+	struct cmsghdr	*cmptr;
+
+        /* assign char* to void* */
+	msg.msg_control = control_un.control;
+	msg.msg_controllen = sizeof(control_un.control);
+#else
+        /* older implementations */
+	int     newfd;
+	msg.msg_accrights = (caddr_t) &newfd;
+	msg.msg_accrightslen = sizeof(int);
+#endif
+
+	msg.msg_name = NULL;
+	msg.msg_namelen = 0;
+
+	iov[0].iov_base = ptr;
+	iov[0].iov_len = nbytes;
+	msg.msg_iov = iov;
+	msg.msg_iovlen = 1;
+
+        /* receive the descriptor in msg through the Unix domain socket fd.
+         * It is normal for the descriptor number in the receiving process
+         * to differ from the descriptor number in the sending process. Passing
+         * a descriptor is not passing a descriptor number, but involves creating
+         * a new descriptor in the receiving process that refers to the same file
+         * table entry within the kernel as the descriptor that was sent by
+         * the sending process.
+*/
+	if ( ( n = recvmsg( fd, &msg, 0)) <= 0)
+		return(n);
+
+#ifdef	HAVE_MSGHDR_MSG_CONTROL
+
+	if ( ( cmptr = CMSG_FIRSTHDR(&msg)) != NULL &&
+	    cmptr->cmsg_len == CMSG_LEN(sizeof(int)))
+        {
+		if ( cmptr->cmsg_level != SOL_SOCKET)
+			err_quit( "control level != SOL_SOCKET");
+                
+		if ( cmptr->cmsg_type != SCM_RIGHTS)
+			err_quit( "control type != SCM_RIGHTS");
+                
+                /* OK, copy descriptor*/
+		*recvfd = *((int *) CMSG_DATA(cmptr));
+	} else
+		*recvfd = -1;		/* descriptor was not passed */
+#else
+
+	if (msg.msg_accrightslen == sizeof(int))
+		*recvfd = newfd;
+	else
+		*recvfd = -1;		/* descriptor was not passed */
+#endif
+
+	return(n);
+}
+/* end read_fd */
+
+ssize_t
+Read_fd( int fd, void *ptr, size_t nbytes, int *recvfd)
+{
+	ssize_t		n;
+
+	if ( ( n = read_fd( fd, ptr, nbytes, recvfd)) < 0)
+		err_sys( "read_fd error");
+
+	return(n);
+}
