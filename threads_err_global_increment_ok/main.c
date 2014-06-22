6cac3f3fd4dc626d8f66ee8ae0d7b238db4567d2
cf16 <cf16rocket@gmail.com>
2014-06-22 19:22:11 +0200

    corrected: incrementing globals with sync pthread_mutex_t

---
+++ threads_err_global_increment_ok/main.c
@@ -0,0 +1,64 @@
+/* 
+ * File:   main.c
+ * Author: piter
+ *
+ * Created on June 22, 2014, 5:28 PM
+ */
+
+#include <stdio.h>
+#include <pthread.h>
+
+#include "unpthreads.h"
+
+#define	NLOOP 500
+
+int				counter;		/* incremented by threads */
+pthread_mutex_t counter_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+void	*doit( void *);
+
+int
+main( int argc, char **argv)
+{
+	pthread_t	tidA, tidB, tidC, tidD;
+
+	Pthread_create( &tidA, NULL, &doit, NULL);
+	Pthread_create( &tidB, NULL, &doit, NULL);
+        Pthread_create( &tidC, NULL, &doit, NULL);
+        Pthread_create( &tidD, NULL, &doit, NULL);
+
+	/* wait for both threads to terminate */
+	Pthread_join( tidA, NULL);
+	Pthread_join( tidB, NULL);
+        Pthread_join( tidC, NULL);
+        Pthread_join( tidD, NULL);
+        
+        printf( "counter: %d\n", counter);
+
+	exit(0);
+}
+
+void *
+doit( void *vptr)
+{
+	int		i, val;
+
+	/*
+	 * Each thread fetches, prints, and increments the counter NLOOP times.
+	 * The value of the counter should increase monotonically (if it was be correct).
+	 */
+
+	for ( i = 0; i < NLOOP; i++) {
+            
+            Pthread_mutex_lock( &counter_mutex);
+            val = counter;
+            printf( "%lu: %d\n", pthread_self(), val + 1);
+                    
+            counter = val + 1;
+            fflush( stdout);
+            Pthread_mutex_unlock( &counter_mutex);
+	}
+
+	return(NULL);
+}
+
