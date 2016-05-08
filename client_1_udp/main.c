9ae70919f2d85ce0e2402daa88ae7ebcb4c70d10
Piotr Gregor <piotrek.gregor@gmail.com>
2016-05-08 15:51:57 +0100

    udp cli to c

---
+++ client_1_udp/main.c
@@ -0,0 +1,329 @@
+/* 
+ * File:   main.c
+ * Author: Piotr Gregor < piotrek gregor at gmail.com >
+ *
+ * Created on May 22, 2014, 9:02 PM
+ */
+
+#include <stdio.h>
+#include <errno.h>
+#include	<stdarg.h>		/* ANSI C header file */
+#include	<syslog.h>		/* for syslog() */
+#include <string.h>
+#include <stdlib.h>
+
+#include <unistd.h>
+#include	<sys/types.h>
+#include	<sys/socket.h>
+#include	<netinet/in.h>
+#include	<arpa/inet.h>
+
+#include <sys/resource.h>
+
+#define MAXLINE 4096
+#define SERV_PORT 5555
+#define SA struct sockaddr
+int		daemon_proc;		/* set nonzero by daemon_init() */
+/* Print message and return to caller
+ * Caller specifies "errnoflag" and "level" */
+
+static void
+err_doit(int errnoflag, int level, const char *fmt, va_list ap)
+{
+	int		errno_save, n;
+	char	buf[MAXLINE + 1];
+
+	errno_save = errno;		/* value caller might want printed */
+#ifdef	HAVE_VSNPRINTF
+	vsnprintf(buf, MAXLINE, fmt, ap);	/* safe */
+#else
+	vsprintf(buf, fmt, ap);					/* not safe */
+#endif
+	n = strlen(buf);
+	if (errnoflag)
+		snprintf(buf + n, MAXLINE - n, ": %s", strerror(errno_save));
+	strcat(buf, "\n");
+
+	if (daemon_proc) {
+		syslog(level, buf);
+	} else {
+		fflush(stdout);		/* in case stdout and stderr are the same */
+		fputs(buf, stderr);
+		fflush(stderr);
+	}
+	return;
+}
+
+/* Fatal error unrelated to system call
+ * Print message and terminate */
+
+void
+err_quit(const char *fmt, ...)
+{
+	va_list		ap;
+
+	va_start(ap, fmt);
+	err_doit(0, LOG_ERR, fmt, ap);
+	va_end(ap);
+	exit(1);
+}
+
+/* Fatal error related to system call
+ * Print message and terminate */
+
+void
+err_sys(const char *fmt, ...)
+{
+	va_list		ap;
+
+	va_start(ap, fmt);
+	err_doit(1, LOG_ERR, fmt, ap);
+	va_end(ap);
+	exit(1);
+}
+
+void
+Inet_pton(int family, const char *strptr, void *addrptr)
+{
+	int		n;
+
+	if ( (n = inet_pton(family, strptr, addrptr)) < 0)
+		err_sys("inet_pton error for %s", strptr);	/* errno set */
+	else if (n == 0)
+		err_quit("inet_pton error for %s", strptr);	/* errno not set */
+
+	/* nothing to return */
+}
+
+int
+Socket(int family, int type, int protocol)
+{
+	int		n;
+
+	if ( (n = socket(family, type, protocol)) < 0)
+		err_sys("socket error");
+	return(n);
+}
+
+void
+Connect(int fd, const struct sockaddr *sa, socklen_t salen)
+{
+	if (connect(fd, sa, salen) < 0)
+		err_sys("connect error");
+}
+
+ssize_t
+Read(int fd, void *ptr, size_t nbytes)
+{
+	ssize_t		n;
+
+	if ( (n = read(fd, ptr, nbytes)) == -1)
+		err_sys("read error");
+	return(n);
+}
+
+void
+Write(int fd, void *ptr, size_t nbytes)
+{
+	if (write(fd, ptr, nbytes) != nbytes)
+		err_sys("write error");
+}
+
+ssize_t						/* Write "n" bytes to a descriptor. */
+writen(int fd, const void *vptr, size_t n)
+{
+	size_t		nleft;
+	ssize_t		nwritten;
+	const char	*ptr;
+
+	ptr = ( const char*) vptr;
+	nleft = n;
+	while (nleft > 0) {
+		if ( (nwritten = write(fd, ptr, nleft)) <= 0) {
+			if (nwritten < 0 && errno == EINTR)
+				nwritten = 0;		/* and call write() again */
+			else
+				return(-1);			/* error */
+		}
+
+		nleft -= nwritten;
+		ptr   += nwritten;
+	}
+	return(n);
+}
+/* end writen */
+
+void
+Writen(int fd, void *ptr, size_t nbytes)
+{
+	if (writen(fd, ptr, nbytes) != nbytes)
+		err_sys("writen error");
+}
+
+int
+Select(int nfds, fd_set *readfds, fd_set *writefds, fd_set *exceptfds,
+       struct timeval *timeout)
+{
+	int		n;
+
+	if ( (n = select(nfds, readfds, writefds, exceptfds, timeout)) < 0)
+		err_sys("select error");
+	return(n);		/* can return 0 on timeout */
+}
+
+void
+Send(int fd, const void *ptr, size_t nbytes, int flags)
+{
+	if (send(fd, ptr, nbytes, flags) != (ssize_t)nbytes)
+		err_sys("send error");
+}
+
+void
+Sendto(int fd, const void *ptr, size_t nbytes, int flags,
+	   const struct sockaddr *sa, socklen_t salen)
+{
+	if ( sendto( fd, ptr, nbytes, flags, sa, salen) != (ssize_t)nbytes)
+		err_sys("sendto error");
+}
+
+void
+Sendmsg(int fd, const struct msghdr *msg, int flags)
+{
+	unsigned int	i;
+	ssize_t			nbytes;
+
+	nbytes = 0;	/* must first figure out what return value should be */
+	for (i = 0; i < msg->msg_iovlen; i++)
+		nbytes += msg->msg_iov[i].iov_len;
+
+	if (sendmsg(fd, msg, flags) != nbytes)
+		err_sys("sendmsg error");
+}
+
+void
+Setsockopt(int fd, int level, int optname, const void *optval, socklen_t optlen)
+{
+	if (setsockopt(fd, level, optname, optval, optlen) < 0)
+		err_sys("setsockopt error");
+}
+
+void
+Shutdown(int fd, int how)
+{
+	if (shutdown(fd, how) < 0)
+		err_sys("shutdown error");
+}
+
+void
+Fclose(FILE *fp)
+{
+	if (fclose(fp) != 0)
+		err_sys("fclose error");
+}
+
+FILE *
+Fdopen(int fd, const char *type)
+{
+	FILE	*fp;
+
+	if ( (fp = fdopen(fd, type)) == NULL)
+		err_sys("fdopen error");
+
+	return(fp);
+}
+
+char *
+Fgets(char *ptr, int n, FILE *stream)
+{
+	char	*rptr;
+
+	if ( (rptr = fgets(ptr, n, stream)) == NULL && ferror(stream))
+		err_sys("fgets error");
+
+	return (rptr);
+}
+
+FILE *
+Fopen( const char *filename, const char *mode)
+{
+	FILE	*fp;
+
+	if ( ( fp = fopen( filename, mode)) == NULL)
+		err_sys( "fopen error");
+
+	return( fp);
+}
+
+void
+Fputs( const char *ptr, FILE *stream)
+{
+	if ( fputs( ptr, stream) == EOF)
+		err_sys( "fputs error");
+}
+
+ssize_t
+Recvfrom( int fd, void *ptr, size_t nbytes, int flags,
+		 struct sockaddr *sa, socklen_t *salenptr)
+{
+	ssize_t		n;
+
+	if ( ( n = recvfrom( fd, ptr, nbytes, flags, sa, salenptr)) < 0)
+		err_sys( "recvfrom error");
+        printf( "recvfrom n =  %d\n", n);
+	return(n);
+}
+
+void
+dg_cli( FILE *fp, int sockfd, const SA *pservaddr, socklen_t servlen)
+{
+	int	n;
+	char	sendline[MAXLINE], recvline[MAXLINE + 1];
+
+	while ( Fgets( sendline, MAXLINE, fp) != NULL) {
+
+            /* If server is not listening an ICMP will be returned with Type 3 Code 3
+             * destination unreachable, port unreachable. 
+             * 
+             * Host that doesn't listen responds with an ICMP port unreachable.
+             * This ICMP error message contains the IP header and UDP header
+             * of the datagram that caused the error. ( ICMPv4 and ICMPv6 error
+             * messages always contain the IP header and all of the UDP header
+             * or part of the TCP header to allow the receiver of the ICMP
+             * error to determine which socket caused the error)
+             * 
+             * The basic rule is that an asynchronous error is not returned
+             * for a UDP socket unless the socket has been connected. */
+		Sendto(sockfd, sendline, strlen(sendline) - 1, 0, pservaddr, servlen);
+
+		n = Recvfrom(sockfd, recvline, MAXLINE, 0, NULL, NULL);
+
+		recvline[n] = 0;	/* null terminate */
+		Fputs( recvline, stdout);
+                Fputs( "\n", stdout);
+	}
+}
+
+/*
+ * 
+ */
+int
+main(int argc, char **argv)
+{
+	int			sockfd;
+	struct sockaddr_in	servaddr;
+
+	if ( argc != 2)
+		err_quit( "usage: udpcli <IPaddress>");
+
+	bzero( &servaddr, sizeof( servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_port = htons( SERV_PORT);
+	Inet_pton( AF_INET, argv[1], &servaddr.sin_addr);
+
+	sockfd = Socket(AF_INET, SOCK_DGRAM, 0);
+
+	dg_cli( stdin, sockfd, (SA *) &servaddr, sizeof( servaddr));
+
+	exit(0);
+}
+
