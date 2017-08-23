0628c599f20304bad9c8b7bd7e7e70cca1328291
Piotr Gregor <piotrgregor@rsyncme.org>
2017-08-23 22:03:00 +0100

    #fix missing bracket around write...

--- echotcp/main.c
+++ echotcp/main.c
@@ -16,7 +16,7 @@ int main(void)
 	int fd = -1;
 	struct sockaddr_in server = {0};
 	const char *message = "test message";
-	char msg[MSG_LEN + 1] = {0};						/* length of message + terminating '\0' */
+	char msg[MSG_LEN + 1] = {0};			/* length of message + terminating '\0' */
 
 	server.sin_family = AF_INET;
 	server.sin_port = htons(1234);
@@ -32,29 +32,36 @@ int main(void)
 	}
 
 	memcpy(msg, message, MSG_LEN);
-	msg[MSG_LEN + 1] = '\0';							/* just in case */
+	msg[MSG_LEN + 1] = '\0';			/* just in case */
 
 	int bytes_n = 0;
 	int written_now = 0;
-	while ((written_now = write(fd, msg, MSG_LEN) != -1) && ((bytes_n += written_now) < MSG_LEN));
+	while (((written_now = write(fd, msg, MSG_LEN)) != -1)
+	       && ((bytes_n += written_now) < MSG_LEN))
+	{
+		fprintf(stderr, "written %d, total %d\n", written_now, bytes_n);
+	}
+
 	if (written_now == -1) {
 		perror("Error on write");
 		exit(EXIT_FAILURE);
-	}
-	else
+	} else {
 		fprintf(stdout, "Written: %s\n", msg);
+	}
 
 	memset(msg, 0, MSG_LEN);
 
 	bytes_n = 0;
 	int read_now = 0;
-	while (((read_now = read(fd, msg, MSG_LEN)) != -1) && ((bytes_n += read_now) < MSG_LEN));
+	while (((read_now = read(fd, msg, MSG_LEN)) != -1)
+	       && ((bytes_n += read_now) < MSG_LEN));
+
 	if (read_now == -1) {
 		perror("Error on read");
 		exit(EXIT_FAILURE);
-	}
-	else
+	} else {
 		fprintf(stdout, "Read: %s\n", msg);
+	}
 
 	return EXIT_SUCCESS;
 }
